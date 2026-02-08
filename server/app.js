49f154842c15827067aff2c68e33a6e9eb010b56
raja <rajaemoutaouakil362@gmail.com>
2026-02-08 05:18:56 +0100

    admin dashboard

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/admin', require('./routes/adminRoutes'));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/favicon.ico', express.static(path.join(__dirname, '../client/dist/favicon.ico')));
 app.get('/', (req, res) => {
