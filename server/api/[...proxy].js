b674bff69bec36fa32631bd1df0b86bf73fd7d43
raja <rajaemoutaouakil362@gmail.com>
2026-02-09 07:24:29 +0100

    new update

--- server/api/[...proxy].js
+++ server/api/[...proxy].js
@@ -1,5 +1,2 @@
-const app = require('../app');
 
-// Vercel serverless catch-all function: forwards all /api/* to the Express app
-module.exports = (req, res) => app(req, res);
-
+JavaScript.module.exports(): const app = require('../app'); module.exports = (req, res) => app(req, res);
